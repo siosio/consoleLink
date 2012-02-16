6ad27b6d2da3bc3cd9f79c7aae9a49d142c60e32
siosio <siosio12@gmail.com>
2012-02-16 15:11:06 +0900

    first commit

---
+++ src/siosio/ConsoleLinkFilter.java
@@ -0,0 +1,26 @@
+package siosio;
+
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+import com.intellij.execution.filters.Filter;
+import com.intellij.ide.browsers.OpenUrlHyperlinkInfo;
+import com.intellij.openapi.editor.markup.TextAttributes;
+
+public class ConsoleLinkFilter implements Filter {
+
+    private static final Pattern URL_PATTERN = Pattern.compile(
+            "(http[s]?://[\\S]+)");
+
+    @Override
+    public Result applyFilter(String s, int endPoint) {
+        int startPoint = endPoint - s.length();
+        Matcher matcher = URL_PATTERN.matcher(s);
+        if (matcher.find()) {
+            return new Result(startPoint + matcher.start(),
+                    startPoint + matcher.end(), new OpenUrlHyperlinkInfo(matcher.group(1)));
+        } else {
+            return new Result(startPoint, endPoint, null, new TextAttributes());
+        }
+    }
+}
