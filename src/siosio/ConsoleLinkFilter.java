96821c08c22589fa9011fea5a919b5bbcf07b9fd
George Palmer <geo@gopalmer.co.uk>
2014-03-10 15:37:41 +0000

    Added support for multiple links inline and links starting with /.

--- src/siosio/ConsoleLinkFilter.java
+++ src/siosio/ConsoleLinkFilter.java
@@ -1,26 +1,29 @@
 package siosio;
 
+import java.util.ArrayList;
+import java.util.List;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
 import com.intellij.execution.filters.Filter;
 import com.intellij.ide.browsers.OpenUrlHyperlinkInfo;
-import com.intellij.openapi.editor.markup.TextAttributes;
 
 public class ConsoleLinkFilter implements Filter {
 
     private static final Pattern URL_PATTERN = Pattern.compile(
-            "((https?://|file:/{1,3})[-_.!~*\\\\'()a-zA-Z0-9;\\\\/?:\\\\@&=+\\\\$,%#]+)");
+            "((https?://|file:/{1,3}|/)[-_.!~*\\\\'()a-zA-Z0-9;\\\\/?:\\\\@&=+\\\\$,%#]+)");
 
     @Override
     public Result applyFilter(String s, int endPoint) {
         int startPoint = endPoint - s.length();
+        List<ResultItem> results = new ArrayList<ResultItem>();
+
         Matcher matcher = URL_PATTERN.matcher(s);
-        if (matcher.find()) {
-            return new Result(startPoint + matcher.start(),
-                    startPoint + matcher.end(), new OpenUrlHyperlinkInfo(matcher.group(1)));
-        } else {
-            return new Result(startPoint, endPoint, null, new TextAttributes());
+        while (matcher.find()) {
+            results.add(new Result(startPoint + matcher.start(),
+                    startPoint + matcher.end(), new OpenUrlHyperlinkInfo(matcher.group(1))));
         }
+
+        return new Result(results);
     }
 }
--- test/siosio/ConsoleLinkFilterTest.java
+++ test/siosio/ConsoleLinkFilterTest.java
@@ -1,60 +1,127 @@
 package siosio;
 
+import com.intellij.execution.filters.Filter.ResultItem;
 import com.intellij.execution.filters.Filter.Result;
 import org.junit.Test;
+
+import java.util.List;
 
 import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertNotNull;
 import static org.junit.Assert.assertNull;
 
 public class ConsoleLinkFilterTest {
-  ConsoleLinkFilter filter = new ConsoleLinkFilter();
+    ConsoleLinkFilter filter = new ConsoleLinkFilter();
 
-  @Test
-  public void emptyConsole() {
-    Result result = filter.applyFilter("", 0);
-    assertEquals(0, result.highlightStartOffset);
-    assertEquals(0, result.highlightEndOffset);
-    assertNull(result.hyperlinkInfo);
-  }
+    @Test
+    public void emptyConsole() {
+        Result result = filter.applyFilter("", 0);
+        List<ResultItem> results = result.getResultItems();
 
-  @Test
-  public void plainText() {
-    Result result = filter.applyFilter("John Smith", 10);
-    assertEquals(0, result.highlightStartOffset);
-    assertEquals(10, result.highlightEndOffset);
-    assertNull(result.hyperlinkInfo);
-  }
+        for (ResultItem curResult : results) {
+            assertEquals(0, curResult.highlightStartOffset);
+            assertEquals(0, curResult.highlightEndOffset);
+            assertNull(curResult.hyperlinkInfo);
+        }
+    }
 
-  @Test
-  public void httpLink() {
-    Result result = filter.applyFilter("http://selenide.org", 19);
-    assertEquals(0, result.highlightStartOffset);
-    assertEquals(19, result.highlightEndOffset);
-    assertNotNull(result.hyperlinkInfo);
-  }
+    @Test
+    public void plainText() {
+        Result result = filter.applyFilter("John Smith", 10);
+        List<ResultItem> results = result.getResultItems();
 
-  @Test
-  public void httpsLink() {
-    Result result = filter.applyFilter("https://selenide.org", 20);
-    assertEquals(0, result.highlightStartOffset);
-    assertEquals(20, result.highlightEndOffset);
-    assertNotNull(result.hyperlinkInfo);
-  }
+        for (ResultItem curResult : results) {
+            assertEquals(0, curResult.highlightStartOffset);
+            assertEquals(10, curResult.highlightEndOffset);
+            assertNull(curResult.hyperlinkInfo);
+        }
+    }
 
-  @Test
-  public void fileLink() {
-    Result result = filter.applyFilter("file:///tmp/screenshot.png", 26);
-    assertEquals(0, result.highlightStartOffset);
-    assertEquals(26, result.highlightEndOffset);
-    assertNotNull(result.hyperlinkInfo);
-  }
+    @Test
+    public void httpLink() {
+        Result result = filter.applyFilter("http://selenide.org", 19);
+        List<ResultItem> results = result.getResultItems();
 
-  @Test
-  public void fileLinkWithSingleSlash() {
-    Result result = filter.applyFilter("file:/tmp/screenshot.png", 24);
-    assertEquals(0, result.highlightStartOffset);
-    assertEquals(24, result.highlightEndOffset);
-    assertNotNull(result.hyperlinkInfo);
-  }
+        for (ResultItem curResult : results) {
+            assertEquals(0, curResult.highlightStartOffset);
+            assertEquals(19, curResult.highlightEndOffset);
+            assertNotNull(curResult.hyperlinkInfo);
+        }
+    }
+
+    @Test
+    public void httpsLink() {
+        Result result = filter.applyFilter("https://selenide.org", 20);
+        List<ResultItem> results = result.getResultItems();
+
+        for (ResultItem curResult : results) {
+            assertEquals(0, curResult.highlightStartOffset);
+            assertEquals(20, curResult.highlightEndOffset);
+            assertNotNull(curResult.hyperlinkInfo);
+        }
+    }
+
+    @Test
+    public void fileLink() {
+        Result result = filter.applyFilter("file:///tmp/screenshot.png", 26);
+        List<ResultItem> results = result.getResultItems();
+
+        for (ResultItem curResult : results) {
+            assertEquals(0, curResult.highlightStartOffset);
+            assertEquals(26, curResult.highlightEndOffset);
+            assertNotNull(curResult.hyperlinkInfo);
+        }
+    }
+
+    @Test
+    public void fileLinkWithSingleSlash() {
+        Result result = filter.applyFilter("file:/tmp/screenshot.png", 24);
+        List<ResultItem> results = result.getResultItems();
+
+        for (ResultItem curResult : results) {
+            assertEquals(0, curResult.highlightStartOffset);
+            assertEquals(24, curResult.highlightEndOffset);
+            assertNotNull(curResult.hyperlinkInfo);
+        }
+    }
+
+    @Test
+    public void LinkWithSingleSlash() {
+        Result result = filter.applyFilter("/tmp/screenshot.png", 19);
+        List<ResultItem> results = result.getResultItems();
+
+        for (ResultItem curResult : results) {
+            assertEquals(0, curResult.highlightStartOffset);
+            assertEquals(19, curResult.highlightEndOffset);
+            assertNotNull(curResult.hyperlinkInfo);
+        }
+    }
+
+    @Test
+    public void MultiLinks() {
+        Result result = filter.applyFilter(
+                "http://selenide.org Some text " +
+                "file:/tmp/screenshot.png some more text " +
+                "https://gopalmer.co.uk", 92);
+
+        List<ResultItem> results = result.getResultItems();
+
+        //http://selenide.org
+        ResultItem curResult = results.get(0);
+        assertEquals(0, curResult.highlightStartOffset);
+        assertEquals(19, curResult.highlightEndOffset);
+        assertNotNull(curResult.hyperlinkInfo);
+
+        //file:/tmp/screenshot.png
+        curResult = results.get(1);
+        assertEquals(30, curResult.highlightStartOffset);
+        assertEquals(54, curResult.highlightEndOffset);
+        assertNotNull(curResult.hyperlinkInfo);
+
+        //https://gopalmer.co.uk
+        curResult = results.get(2);
+        assertEquals(70, curResult.highlightStartOffset);
+        assertEquals(92, curResult.highlightEndOffset);
+        assertNotNull(curResult.hyperlinkInfo);
+    }
 }
