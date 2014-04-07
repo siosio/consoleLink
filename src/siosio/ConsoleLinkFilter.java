544c7ab0db0fed06cbc44fd9e47ff849660e6221
Andrei Solntsev <andrei.solntsev@gmail.com>
2014-04-07 22:35:00 +0300

    Do not treat any text containing slash (/) as link. Only if space preceeds slash.

--- src/siosio/ConsoleLinkFilter.java
+++ src/siosio/ConsoleLinkFilter.java
@@ -1,29 +1,39 @@
 package siosio;
 
+import com.intellij.execution.filters.Filter;
+import com.intellij.ide.browsers.OpenUrlHyperlinkInfo;
+
 import java.util.ArrayList;
 import java.util.List;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
-import com.intellij.execution.filters.Filter;
-import com.intellij.ide.browsers.OpenUrlHyperlinkInfo;
-
 public class ConsoleLinkFilter implements Filter {
 
-    private static final Pattern URL_PATTERN = Pattern.compile(
-            "((https?://|file:/{1,3}|/)[-_.!~*\\\\'()a-zA-Z0-9;\\\\/?:\\\\@&=+\\\\$,%#]+)");
+    public static final String URI_PATTERN = "((https?://|file:/{1,3}|/)[-_.!~*\\\\'()a-zA-Z0-9;\\\\/?:\\\\@&=+\\\\$,%#\\[\\]]+)";
+    private static final Pattern BEGINNING_OF_LINE = Pattern.compile("^" + URI_PATTERN);
+    private static final Pattern IN_THE_MIDDLE_OF_LINE = Pattern.compile("\\s" + URI_PATTERN);
 
     @Override
-    public Result applyFilter(String s, int endPoint) {
-        int startPoint = endPoint - s.length();
+    public Result applyFilter(String textLine, int endPoint) {
+        int startPoint = endPoint - textLine.length();
         List<ResultItem> results = new ArrayList<ResultItem>();
 
-        Matcher matcher = URL_PATTERN.matcher(s);
-        while (matcher.find()) {
-            results.add(new Result(startPoint + matcher.start(),
-                    startPoint + matcher.end(), new OpenUrlHyperlinkInfo(matcher.group(1))));
-        }
+        match(results, BEGINNING_OF_LINE, textLine, startPoint);
+        match(results, IN_THE_MIDDLE_OF_LINE, textLine, startPoint);
 
         return new Result(results);
+    }
+
+    private void match(List<ResultItem> results, Pattern pattern, String textLine, int startPoint) {
+        Matcher matcher = pattern.matcher(textLine);
+        while (matcher.find()) {
+            String link = matcher.group(1);
+            results.add(new Result(
+                    startPoint + matcher.start(1),
+                    startPoint + matcher.end(1),
+                    new OpenUrlHyperlinkInfo(link)
+            ));
+        }
     }
 }
--- test/siosio/ConsoleLinkFilterTest.java
+++ test/siosio/ConsoleLinkFilterTest.java
@@ -1,5 +1,6 @@
 package siosio;
 
+import com.intellij.execution.filters.Filter;
 import com.intellij.execution.filters.Filter.Result;
 import com.intellij.execution.filters.Filter.ResultItem;
 import com.intellij.ide.browsers.OpenUrlHyperlinkInfo;
@@ -24,27 +25,29 @@ public class ConsoleLinkFilterTest {
 
     @Test
     public void httpLink() {
-        assertLink("http://selenide.org", "http://selenide.org");
+        assertLink("http://selenide.org", 0, "http://selenide.org");
+        assertLink("   http://selenide.org", 3, "http://selenide.org");
     }
 
     @Test
     public void httpsLink() {
-        assertLink("https://selenide.org", "https://selenide.org");
+        assertLink("https://selenide.org", 0, "https://selenide.org");
+        assertLink("   https://selenide.org", 3, "https://selenide.org");
     }
 
     @Test
     public void fileLink() {
-        assertLink("file:///tmp/screenshot.png", "file:///tmp/screenshot.png");
+        assertLink("file:///tmp/screenshot.png", 0, "file:///tmp/screenshot.png");
     }
 
     @Test
     public void fileLinkWithSingleSlash() {
-        assertLink("file:/tmp/screenshot.png", "file:/tmp/screenshot.png");
+        assertLink("file:/tmp/screenshot.png", 0, "file:/tmp/screenshot.png");
     }
 
     @Test
     public void localFile() {
-        assertLink("/tmp/screenshot.png", "/tmp/screenshot.png");
+        assertLink("/tmp/screenshot.png", 0, "/tmp/screenshot.png");
     }
 
     @Test
@@ -55,9 +58,41 @@ public class ConsoleLinkFilterTest {
                 "https://gopalmer.co.uk", 92);
 
         List<ResultItem> results = result.getResultItems();
+        assertEquals(3, results.size());
         assertResult(results.get(0), 0, "http://selenide.org");
         assertResult(results.get(1), 30, "file:/tmp/screenshot.png");
         assertResult(results.get(2), 70, "https://gopalmer.co.uk");
+    }
+
+    @Test
+    public void linkShouldBePrefixedWithSeparator() {
+        String link = " http://www.ee http://google.com bla?http://not.me";
+        Result result = filter.applyFilter(link, link.length());
+        List<ResultItem> results = result.getResultItems();
+        assertEquals(2, results.size());
+        assertResult(results.get(0), 1, "http://www.ee");
+        assertResult(results.get(1), 15, "http://google.com");
+    }
+
+    @Test
+    public void urlWithSquareBraces() {
+        String link = "http://ci.org/job/123/artifact/build/reports/checksDialogext[1]/1394746367500.10.png";
+        String textLine = "Screenshot: http://ci.org/job/123/artifact/build/reports/checksDialogext[1]/1394746367500.10.png";
+        Filter.Result result = filter.applyFilter(textLine, textLine.length());
+        List<Filter.ResultItem> results = result.getResultItems();
+
+        assertEquals(1, results.size());
+        assertLink(textLine, "Screenshot: ".length(), link);
+    }
+
+    @Test
+    public void urlWithBraces() {
+        assertNoLinksMatched("Module web is available (/home/xp/work/play/./modules/web-1.4.6)");
+    }
+
+    @Test
+    public void consoleOutputWithSlashesButNotLinks() {
+        assertNoLinksMatched("INFO 13.03.14 9:50:liquibase: db/custom.xml: db/generic.xml::prevent_ddl_locking::Codeborne: Custom SQL executed");
     }
 
     private void assertNoLinksMatched(String textLine) {
@@ -67,13 +102,13 @@ public class ConsoleLinkFilterTest {
         assertEquals(0, results.size());
     }
 
-    private void assertLink(String expectedUrl, String textLine) {
+    private void assertLink(String textLine, int startIndex, String expectedUrl) {
         Result result = filter.applyFilter(textLine, textLine.length());
         List<ResultItem> results = result.getResultItems();
 
         assertEquals(1, results.size());
         ResultItem item = results.get(0);
-        assertResult(item, 0, expectedUrl);
+        assertResult(item, startIndex, expectedUrl);
     }
 
     private void assertResult(ResultItem item, int startIndex, String expectedUrl) {
